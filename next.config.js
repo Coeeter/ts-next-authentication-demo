611753e4417142af6b38b9009415a5ce1919a807
Nasrullah <nasrullah01n@gmail.com>
2023-03-09 23:20:53 +0800

    Added documentation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => [
+    {
+      source: "/",
+      destination: "/redoc-static.html",
+    },
+  ],
 }
 
 module.exports = nextConfig
